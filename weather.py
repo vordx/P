b37d747d7fc0d82a936773a68a1c684534e8d91b
vordx <39927714+vordx@users.noreply.github.com>
2020-12-11 21:25:03 +0200

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,26 @@
+﻿import requests
+from .. import loader, utils
+
+
+def register(cb):
+    cb(WeatherMod())
+    
+class WeatherMod(loader.Module):
+    """Погода с сайта wttr.in"""
+    strings = {'name': 'Weather'}
+    
+    async def pwcmd(self, message):
+        """"Кидает погоду картинкой.\nИспользование: .pw <город>; ничего."""
+        args = utils.get_args_raw(message).replace(' ', '+')
+        await message.edit("Узнаем погоду...")
+        city = requests.get(f"https://wttr.in/{args if args != None else ''}.png").content
+        await message.client.send_file(message.to_id, city)
+        await message.delete()
+
+
+    async def awcmd(self, message):
+        """Throws weather ascii-artom.\reusing the art: .aw <City>; Nothing."""
+        city = utils.get_args_raw(message)
+        await message.edit("Finding weather...")
+        r = requests.get(f"https://wttr.in/{city if city != None else ''}?0?q?T&lang=en")
+        await message.edit(f"<code>City: {r.text}</code>")
